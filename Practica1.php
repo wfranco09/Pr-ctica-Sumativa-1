bc9e3aac659601c0ec325dddcec4c26c5a28dd2c
Winston Franco <winstonfranco56@gmail.com>
2026-04-05 14:48:46 -0500

    Practica actualizada

---
+++ Practica1.php
@@ -0,0 +1,54 @@
+<?php
+echo "Problema 1<br>";
+echo "<br>SUMA";
+$num1 = 3;
+$num2 = 5;
+$resultado = $num1 + $num2 ;
+echo "<br>El Resultado de $num1 + $num2 es:  $resultado" ;
+
+echo "<br><br>RESTA";
+$num1 = 9;
+$num2 = 1;
+$resultado = $num1 - $num2 ;
+echo "<br>El Resultado de $num1 - $num2 es:  $resultado" ;
+
+echo "<br><br>Multiolicación";
+$num1 = 9;
+$num2 = 9;
+$resultado = $num1 * $num2 ;
+echo "<br>El Resultado de $num1 * $num2 es:  $resultado" ;
+
+echo "<br><br>Multiplicación";
+$num1 = 9;
+$num2 = 9;
+$resultado = $num1 / $num2 ;
+echo "<br>El Resultado de $num1 / $num2 es:  $resultado" ;
+
+echo "<br><br>Módulo";
+$num1 = 4;
+$num2 = 8;
+$resultado = $num1 % $num2 ;
+echo "<br>El Resultado del modulo de $num1 entre $num2 es:  $resultado" ;
+
+echo "<br><br>Potencia";
+$num1 = 2;
+$num2 = 2;
+$resultado = $num1 ** $num2 ;
+echo "<br>El Resultado de la potencia de $num1 a la $num2 es: $resultado" ;
+
+echo "<br><br>Orden de Procedencia";
+$num1 = 2;
+$num2 = 2;
+$num3 = 3;
+$resultado = $num1 + $num2 * $num3;
+echo "<br> El resultado de Orden de procedencia es : $resultado" ;
+
+echo "<br><br>Orden de Procedencia";
+$num1 = 2;
+$num2 = 2;
+$num3 = 3;
+$resultado = ($num1 + $num2) * $num3;
+echo "<br> El resultado de Orden de procedencia es : $resultado"     ;
+
+echo "<br><br>HOLA MUNDO";
+?>
--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# Pr-ctica-Sumativa-1
+# Practica-Sumativa-1
+Este proyecto es una práctica introductoria en PHP donde se desarrollan distintas operaciones aritméticas básicas utilizando variables y operadores. Los resultados se muestran directamente en pantalla mediante el uso de echo.
+
+Objetivo de la práctica
+
+El propósito de esta práctica es reforzar los siguientes conceptos fundamentales:
+
+- Uso de variables en PHP
+- Aplicación de operadores aritméticos
+- Impresión de resultados en pantalla
+- Comprensión del orden de precedencia en las operaciones matemáticas
+Este repositorio contiene varios ejemplos prácticos en PHP para aprender y reforzar conceptos fundamentales, tales como:
+
+🔹 Variables
+🔹 Arreglos
+🔹 Funciones
+
+🌐 Tecnologías utilizadas
+PHP Apache MySQL XAMPP WampServer
+
